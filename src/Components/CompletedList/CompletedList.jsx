f8dd2a38975503459a6cb2043d38fd52ed84e20a
Natalle <natallemoura7@gmail.com>
2022-01-11 18:17:21 +0100

    Clean unused imports

--- src/Components/CompletedList/CompletedList.jsx
+++ src/Components/CompletedList/CompletedList.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './completed-list.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function CompletedList(props) {
   return (
