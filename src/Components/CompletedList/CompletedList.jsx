48e78caca1408f9e29ae60f86e1440a50ca20540
Natalle <natallemoura7@gmail.com>
2022-01-19 21:52:26 +0100

    Add button to delete

--- src/Components/CompletedList/CompletedList.jsx
+++ src/Components/CompletedList/CompletedList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './completed-list.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function CompletedList(props) {
   return (
@@ -7,14 +9,12 @@ export default function CompletedList(props) {
       <h4 className='bought-list-title'>BOUGHT ITEMS</h4>
       <ul className='bought-list'>
         {props.items.map((item) => <li className='bought-item' key={item.id}>
-          <span>
+          <span className='bought-item-properties'>
             <input type="checkbox" value={props.checked} defaultChecked={item.isCompleted} onChange={() => props.toggle(item)} />
+            <span className='bought-item-name'>{item.name}</span><span>{item.price}:-</span>
           </span>
-          <span className='bought-item-name'>
-          {item.name}
-          </span>
-          <span>
-          {item.price}:-
+          <span className='delete-item-container-completed'>
+            <button onClick={() => props.remove(item)} className='delete-item-button'><FontAwesomeIcon icon={faTrash} /></button>
           </span>
         </li>)}
       </ul>
--- src/Components/ShoppingList/ShoppingList.jsx
+++ src/Components/ShoppingList/ShoppingList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './shopping-list.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function ShoppingList(props) {
   return (
@@ -14,9 +16,11 @@ export default function ShoppingList(props) {
             <li className='shop-list-item' key={item.id}>
               <span>
                 <input type="checkbox" value={props.checked} defaultChecked={item.isCompleted} onChange={() => props.toggle(item)} />
+                <span className='shop-list-item-name'>{item.name}</span><span className='shop-list-item-price'>{item.price}:-</span>
               </span>
-              <span className='shop-list-item-name'>{item.name}</span><span className='shop-list-item-price'>{item.price}:-</span>
-              <button onClick={() => props.remove(item)} >Delete</button>
+              <span className='delete-item-button-container'>
+                <button onClick={() => props.remove(item)} className='delete-item-button'><FontAwesomeIcon icon={faTrash} /></button>
+              </span>
             </li>)}
         </ul>
       </section>
