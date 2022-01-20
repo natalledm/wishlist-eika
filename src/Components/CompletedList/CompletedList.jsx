9ca92d01d55c076454cf1bca857a64fb02e04ef9
Natalle <natallemoura7@gmail.com>
2022-01-20 11:10:12 +0100

    Change CSS to align the delete button

--- src/Components/CompletedList/CompletedList.jsx
+++ src/Components/CompletedList/CompletedList.jsx
@@ -9,11 +9,11 @@ export default function CompletedList(props) {
       <h4 className='bought-list-title'>BOUGHT ITEMS</h4>
       <ul className='bought-list'>
         {props.items.map((item) => <li className='bought-item' key={item.id}>
-          <span className='bought-item-properties'>
+          <span>
             <input type="checkbox" value={props.checked} defaultChecked={item.isCompleted} onChange={() => props.toggle(item)} />
             <span className='bought-item-name'>{item.name}</span><span>{item.price}:-</span>
           </span>
-          <span className='delete-item-container-completed'>
+          <span>
             <button onClick={() => props.remove(item)} className='delete-item-button'><FontAwesomeIcon icon={faTrash} /></button>
           </span>
         </li>)}
